16d84adabfce437c576dd5e75fb494af3451c3bd
gkista <gkista@agroknow.gr>
2013-02-28 18:19:02 +0200

    parse ontology from moki display also individuals

--- application/views/scripts/ontology/transform_owl_to_internal_xml.php
+++ application/views/scripts/ontology/transform_owl_to_internal_xml.php
@@ -1,71 +1,89 @@
-<?php 
+<?php
+
 ob_start();
-$output='<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+$output = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 $output .= '<classification>' . "\n";
 
 ///////get the OWL file////////////////
 libxml_use_internal_errors(false);
-$xmlobj = @simplexml_load_file('http://'.$_SERVER['SERVER_NAME'].''.  uri('ontology/olontology').'');
+$xmlobj = @simplexml_load_file('http://' . $_SERVER['SERVER_NAME'] . '' . uri('ontology/olontology') . '');
 
 //$xml->registerXPathNamespace("owl","http://localhost/TestRequestOntology.php");
-if ($xmlobj===false)
+if ($xmlobj === false)
     die('Bad XML response!');
+
+
+$output .= '<hierarchy rootElement="#OAAEConcept">' . "\n";
+
 /////get all elements with the name:'owl:Class'
 $result = $xmlobj->xpath("owl:Class");
- $i=0;
- 
-$output .= '<hierarchy rootElement="#OAAEConcept">' . "\n";
-foreach($result as $c){ 
-$rdf_elements=$c->children('rdf',true);
-$rdfs_elements=$c->children('rdfs',true);
-$owl_elements=$c->children('owl',true);
+$i = 0;
+
+foreach ($result as $c) {
+    $rdf_elements = $c->children('rdf', true);
+    $rdfs_elements = $c->children('rdfs', true);
+    $owl_elements = $c->children('owl', true);
 
 
-if($rdfs_elements->subClassOf){
-	$i+=1;
-	$sub_owl_elements=$rdfs_elements->subClassOf->children('owl',true);
-$attofsubelement=$sub_owl_elements->attributes('rdf',true);
-$attofelement=$c->attributes('rdf',true);
+    if ($rdfs_elements->subClassOf) {
+        $i+=1;
+        $sub_owl_elements = $rdfs_elements->subClassOf->children('owl', true);
+        $attofsubelement = $sub_owl_elements->attributes('rdf', true);
+        $attofelement = $c->attributes('rdf', true);
 //print $i.'. Element: '.$attofelement['about'].' - Subclass: '.$attofsubelement['about'];
-$output .= '<class id="'.$attofelement['about'].'" className="'.$attofelement['about'].'" subClassOf="'.$attofsubelement['about'].'" />' . "\n";
+        $output .= '<class id="' . $attofelement['about'] . '" className="' . $attofelement['about'] . '" subClassOf="' . $attofsubelement['about'] . '" />' . "\n";
+    }
+}
+$result2 = $xmlobj->xpath("rdf:Description");
+foreach ($result2 as $c2) {
+    $attofelement = $c2->attributes('rdf', true);
+
+    $rdf_elements2 = $c2->children('rdf', true);
+    foreach ($rdf_elements2 as $rdf_elements2) {
+        $owl_elements2 = $rdf_elements2->children('owl', true);
+        foreach ($owl_elements2 as $owl_elements2) {
+            $attofsubelement = $owl_elements2->attributes('rdf', true);
+            //print '. Element: '.$attofelement['about'].' - Subclass: '.$attofsubelement['about'];
+            $output .= '<class id="'.$attofelement['about'].'" className="'.$attofelement['about'].'" subClassOf="'.$attofsubelement['about'].'" />' . "\n";
+        }
+    }
 }
 
-}
+
 $output .= '</hierarchy>' . "\n";
 $output .= '<instances>' . "\n";
 
-foreach($result as $c){ 
+foreach ($result as $c) {
 
 
-$rdf_elements=$c->children('rdf',true);
-$rdfs_elements=$c->children('rdfs',true);
-$owl_elements=$c->children('owl',true);
+    $rdf_elements = $c->children('rdf', true);
+    $rdfs_elements = $c->children('rdfs', true);
+    $owl_elements = $c->children('owl', true);
 
 
-if($owl_elements->hasTranslation){
-	
-	$attofelement=$c->attributes('rdf',true);
-	//echo $attofelement['about'].' - ';
-	$attofelement2=$owl_elements->attributes();
-	//echo $attofelement2['lang'].' - ';
-	//print($owl_elements);
-	$output .= '<instance instanceOf="'.$attofelement['about'].'" lang="'.$attofelement2['lang'].'">' . "";
-	$output .= ''.$owl_elements.'' . "";
-	$output .= '</instance>' . "\n";
-}
+    if ($owl_elements->hasTranslation) {
 
+        $attofelement = $c->attributes('rdf', true);
+        //echo $attofelement['about'].' - ';
+        $attofelement2 = $owl_elements->attributes();
+        //echo $attofelement2['lang'].' - ';
+        //print($owl_elements);
+        $output .= '<instance instanceOf="' . $attofelement['about'] . '" lang="' . $attofelement2['lang'] . '">' . "";
+        $output .= '' . $owl_elements . '' . "";
+        $output .= '</instance>' . "\n";
+    }
 }
 $output .= '</instances>' . "\n";
 
 $output .= '</classification>' . "\n";
 //echo $output;
 /*
-print_r($c->children('owl',true));
-echo "<br>";
-print_r($c->children('rdf',true));
-echo "<br>";
-print_r($c->children('rdfs',true));
-*/
+  print_r($c->children('owl',true));
+  echo "<br>";
+  print_r($c->children('rdf',true));
+  echo "<br>";
+  print_r($c->children('rdfs',true));
+ */
 //echo "<br><br>";
 //if($i>2){break;}
 //if ($xmlobj===false){die('Bad XML!'); }
@@ -76,13 +94,12 @@ print_r($c->children('rdfs',true));
 ob_end_flush();
 //echo "http://".$_SERVER['SERVER_NAME']."".uri('archive/xmlvoc')."/output.xml";
 
-$xmlname="new_oe_ontology_hierrarchy.xml";
+$xmlname = "new_oe_ontology_hierrarchy.xml";
 $fp = fopen("/var/www/html/".uri('archive/xmlvoc')."/new_oe_ontology_hierrarchy.xml","w");
-//$fp = fopen("C:/Program Files (x86)/EasyPHP-12.1/www/".uri('archive/xmlvoc')."/".$xmlname."","w");
-fwrite($fp,$output);
+//$fp = fopen("C:/Program Files (x86)/EasyPHP-12.1/www/" . uri('archive/xmlvoc') . "/" . $xmlname . "", "w");
+fwrite($fp, $output);
 fclose($fp);
-if($fp){
-echo "<a href='http://".$_SERVER['SERVER_NAME']."".uri('archive/xmlvoc')."/".$xmlname."' target='_blank'> New Internal Xml from API ontology</a>";
+if ($fp) {
+    echo "<a href='http://" . $_SERVER['SERVER_NAME'] . "" . uri('archive/xmlvoc') . "/" . $xmlname . "' target='_blank'> New Internal Xml from API ontology</a>";
 }
-
 ?>
