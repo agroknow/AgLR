97cb5be6de76c4ef7c67516301ad25b491116980
gkista <gkista@agroknow.gr>
2014-03-19 17:46:42 +0200

    [login-page] link changes

--- admin/themes/default/common/login-footer.php
+++ admin/themes/default/common/login-footer.php
@@ -1,7 +1,7 @@
 </div>
 <div id="footer">
     <?php admin_plugin_footer(); ?>
-    <p><a href="http://omeka.org" id="omeka-logo"><?php echo __('Powered by Omeka'); ?></a> | <?php echo __('Version %s', OMEKA_VERSION); ?></p>    
+    <p><a href="http://omeka.org" id="omeka-logo"><?php echo __('Powered by Omeka'); ?></a> | <a href="http://www.agroknow.gr" target="_blank" id="omeka-logo"><?php echo __('Developed by AK'); ?></a> <?php //echo __('Version %s', OMEKA_VERSION); ?></p>    
 </div>
 </div>
 </body>
--- application/views/scripts/users/login.php
+++ application/views/scripts/users/login.php
@@ -6,7 +6,8 @@ head(array('bodyclass' => 'login', 'title' => $pageTitle), $header);
 <h1><?php echo $pageTitle; ?></h1>
 
 <p id="login-links">
-<span id="backtosite"><?php echo link_to_home_page(__('Go to Home Page')); ?></span>  |  <span id="forgotpassword"><?php echo link_to('users', 'forgot-password', __('Lost your password?')); ?></span>
+<?php /*<span id="backtosite"><?php echo link_to_home_page(__('Go to Home Page')); ?></span>  |  <span id="forgotpassword"><?php echo link_to('users', 'forgot-password', __('Lost your password?')); ?></span> */ ?>
+<span id="backtosite"><?php echo "<a href='http://wiki.agroknow.gr/agroknow/index.php/AgLR' target='_blank'>".__('Learn about AgLR')."</a>"; ?></span>  |  <span id="forgotpassword"><?php echo link_to('users', 'forgot-password', __('Lost your password?')); ?></span>
 </p>
 
 <?php echo flash(); ?>
